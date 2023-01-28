87c97e52ded3bb661f6d5562d79f658de31f3537
Pawee30948 <pawee30948@yahoo.co.th>
2023-01-28 12:27:16 +0700

    after Pirate Bat game dev meet up 1

--- main.lua
+++ main.lua
@@ -36,8 +36,8 @@ gameGroup:insert( streetBackground2 )
 
 -- Add the movement of the rectangle
 local function moveStreetBackground()
-    streetBackground1.x = streetBackground1.x - 2
-    streetBackground2.x = streetBackground2.x - 2
+    streetBackground1.x = streetBackground1.x - 6
+    streetBackground2.x = streetBackground2.x - 6
     if streetBackground1.x < -streetBackground1.contentWidth/2 then
         streetBackground1.x = streetBackground2.x + streetBackground2.contentWidth
     end
@@ -51,6 +51,18 @@ local player = display.newImageRect( "player.png", 50, 50 )
 player.x = 100
 player.y = display.contentCenterY
 gameGroup:insert( player )
+
+frames = { "PlayerAnimated/frame1.png", "PlayerAnimated/frame2.png", "PlayerAnimated/frame3.png", "PlayerAnimated/frame4.png", "PlayerAnimated/frame5.png", "PlayerAnimated/frame6.png" }
+--local playerAnimated = display.newAnimation(frames,2)
+
+--for i=1,6 do
+ --   playerAnimated:setFrame( i, graphics.newImageSheet("PlayerAnimated/frame" .. i .. ".png") )
+--end
+
+--playerAnimated.looping=true
+--sprite:play(playerAnimated)
+
+local NPCBike = {}
 
 local playerLocationAtSlot = 1
 
@@ -82,13 +94,58 @@ moveDownButton.y = display.contentHeight*(3/4)
 gameGroup:insert( moveDownButton )
 moveDownButton:addEventListener( "tap", moveDown )
 
+local function addNewNPC()
+    local newNPC =    display.newImageRect( "player.png", 50, 50 )
+    math.randomseed(os.time())
+    local NPClaneSlot = math.random(1, 4)
+    newNPC.y = 100 + (50*(NPClaneSlot-1))
+    newNPC.x = math.random(480)
+    table.insert(NPCBike,newNPC);
+    
+    
+end
+
+local NPCSpawnTimer=0
+
+local function checkIfAnyNPCExitScreen()
+    local loopIterator=0
+    if #NPCBike > 0 then
+        while loopIterator < #NPCBike do
+           -- if NPCBike[loopIterator].x < -50 then
+                --table.remove(NPCBike,loopIterator)
+            --    NPCBike[loopIterator].x = 480+50
+            --end
+            NPCBike[loopIterator].x = NPCBike[loopIterator].x - 2
+            
+            loopIterator=loopIterator+1
+        end
+    end
+end
+
+local function moveNPCToScreenLeft()
+
+end
 
 -- Create a function to handle updating our game objects
 local function gameLoop()
 	moveStreetBackground()
 
+    if #NPCBike < 30 then
+     if NPCSpawnTimer<30 then
+         NPCSpawnTimer=NPCSpawnTimer+1
+     else
+           NPCSpawnTimer=0
+           addNewNPC()
+        end
+    end
+
+    moveNPCToScreenLeft()
+
+    checkIfAnyNPCExitScreen()
+
    local _targetPlayerYlocation = 100 + (50 * (playerLocationAtSlot-1))
-   player.y=player.y+((_targetPlayerYlocation-player.y)*(1/30))
+
+    player.y=player.y+((_targetPlayerYlocation-player.y)*(1/30))
    
 end
 
